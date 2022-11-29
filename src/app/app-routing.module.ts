e20b7e2164bce4f3edf3ee3069b11a27caab1b24
sesigali <eduardo.f@aluno.ifsp.edu.br>
2022-11-29 04:21:50 +0000

    commit segundo cod total

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import { BcoService } from './bco.service';
+
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { LivrosComponent } from './livros/livros.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EditarComponent } from './editar/editar.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+
+
+const routes: Routes = [
+  {path:"usuarios", component:UsuariosComponent},
+  {path:"livros", component:LivrosComponent},
+  {path:"cadastroLivro", component:CadastroComponent},
+  {path:"cadastroUsuario", component:CadastroUsuarioComponent},
+  {path:"editarLivro/:isbnLivro/:autorLivro/:tituloLivro/:qtdExemplares/:_id", component:EditarComponent},
+  {path:"editarUsuario/:cpf/:nome/:email/:telefone/:qtdLivrosEmprestados/:_id", component:EditarUsuarioComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)] 
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  constructor(
+    private rota: ActivatedRoute,
+    private local: Location,
+    private servico: BcoService
+  ){}
+ }
